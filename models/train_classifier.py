f7f9408d4558035afef25ec43ff5c8ce3fe78018
Valentina Ceriani <v.ceriani92@gmail.com>
2019-11-15 00:06:44 +0100

    feat: train_classifier

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,117 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
+
+import nltk
+nltk.download(['punkt', 'wordnet', 'stopwords'])
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.metrics import classification_report
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.model_selection import GridSearchCV
+import pickle
+
+_data_connection_prefix = 'sqlite:///'
 
 
 def load_data(database_filepath):
-    pass
+    '''
+    INPUT:
+    database_filepath - filepath to the database with cleaned data
+    
+    OUTPUT:
+    X - pandas DataFrame with model input data
+    Y - pandas DataFrame with model response data
+    column names - column names for response data
+    '''
+    
+    engine = create_engine(_data_connection_prefix + database_filepath)
+    df = pd.read_sql_table('messages_with_categories', engine)
+    X = df['message']
+    Y = df.iloc[:, 4:]
+    return X, Y, Y.columns
 
 
 def tokenize(text):
-    pass
+    '''
+    INPUT:
+    text - message
 
+    OUTPUT:
+    tokens - list with all text tokens in the original message
+    '''
+    
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    stop_words = stopwords.words("english")
+
+    clean_tokens = []
+    for tok in tokens:
+        # lemmatize, normalize case, and remove leading/trailing white space
+        clean_tok = lemmatizer.lemmatize(tok.lower().strip())
+        if clean_tok not in stop_words:
+            clean_tokens.append(clean_tok)
+
+    return clean_tokens
+    
 
 def build_model():
-    pass
+    '''
+    OUTPUT:
+    model - a model which has been optimized for the global task by using GridSearchCV
+    '''
+    
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(estimator=RandomForestClassifier()))
+    ])
+
+    parameters = {
+        'vect__ngram_range': ((1, 1), (1, 2)),
+        'vect__max_df': (0.5, 0.75, 1.0),
+        'vect__max_features': (None, 5000, 10000),
+        'tfidf__use_idf': (True, False),
+        'clf__estimator__n_estimators': [50, 100, 200],
+        'clf__estimator__min_samples_split': [2, 3, 4]
+    }
+
+    cv = GridSearchCV(pipeline, param_grid=parameters, verbose=2)
+    
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    '''
+    INPUT:
+    model - model to be evaluated
+    X_test - test input data
+    Y_test - test response data
+    category_names - column names for response data
+    '''
+    
+    Y_pred = model.predict(X_test)
+    Y_pred_df = pd.DataFrame(Y_pred, columns=category_names)
+    
+    for col in category_names:
+        print('Test results for "' + col + '" :\n')
+        print(classification_report(Y_test.loc[:, col], Y_pred_df.loc[:, col]))
+        print('\n')
 
 
 def save_model(model, model_filepath):
-    pass
+    '''
+    INPUT:
+    model - model to be saved as pkl
+    model_filepath - filepath and model file name
+    '''
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
